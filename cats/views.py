a3d592326e9551aa7bd566ba6cc1a659f1fe48f9
Konstantin Khakhilev <constantin.khakhilev@gmail.com>
2023-12-24 20:05:17 +0700

    Тема 4/10: Django Rest Framework → Урок 7/15

--- cats/views.py
+++ cats/views.py
@@ -1,30 +1,15 @@
 # Обновлённый views.py
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 
 from .models import Cat
 from .serializers import CatSerializer
 
 
-class APICat(APIView):
-    def get(self, request):
-        cats = Cat.objects.all()
-        serializer = CatSerializer(cats, many=True)
-        return Response(serializer.data)
+class CatList(generics.ListCreateAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
 
-    def post(self, request):
-        # Создаём объект сериализатора 
-        # и передаём в него данные из POST-запроса
-        # many=True указывает, что ожидается список объектов
-        serializer = CatSerializer(data=request.data)
-        if serializer.is_valid():
-            # Если полученные данные валидны —
-            # сохраняем данные в базу через save().
-            serializer.save()
-            # Возвращаем JSON со всеми данными нового объекта
-            # и статус-код 201
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        # Если данные не прошли валидацию — 
-        # возвращаем информацию об ошибках и соответствующий статус-код:
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
